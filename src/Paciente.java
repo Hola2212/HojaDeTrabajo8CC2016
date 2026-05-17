d6666b5506638abfa5f6b59f2f798463b4cae96e
Hola2212 <juanpaflores06@gmail.com>
2026-05-17 12:18:30 -0600

    Desarrollo de clase Paciente.

--- src/Paciente.java
+++ src/Paciente.java
@@ -1,2 +1,56 @@
-public class Paciente {
+public class Paciente implements Comparable<Paciente>{
+
+    private String nombre;
+    private String sintoma;
+    private char prioridad;
+
+    public Paciente(String nombre, String sintoma, char prioridad) {
+        this.nombre = nombre;
+        this.sintoma = sintoma;
+        this.prioridad = prioridad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+
+    public void setSintoma(String sintoma){
+        this.sintoma = sintoma;
+    }
+
+    public void setPrioridad(char prioridad){
+        this.prioridad = prioridad;
+    }
+
+    public String getSintoma() {
+        return sintoma;
+    }
+
+    public char getPrioridad() {
+        return prioridad;
+    }
+
+    @Override
+    public int compareTo(Paciente other) {
+
+        return Character.compare(
+                this.prioridad,
+                other.prioridad
+        );
+    }
+
+    @Override
+    public String toString() {
+
+        return nombre +
+                ", " +
+                sintoma +
+                ", " +
+                prioridad;
+    }
+
 }
