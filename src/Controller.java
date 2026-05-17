1b50e9f4b767985234de9fd922bb6380076ceb4e
Hola2212 <juanpaflores06@gmail.com>
2026-05-17 15:51:32 -0600

    Completed main and Controller. Added example text.

--- src/Controller.java
+++ src/Controller.java
@@ -1,2 +1,26 @@
+import java.util.Vector;
+
 public class Controller {
+
+    private IPriorityQueue<Paciente> queue;
+    private FileReaderUtil reader;
+
+    public Controller(IPriorityQueue<Paciente> queue) {
+        this.queue = queue;
+        reader = new FileReaderUtil();
+    }
+
+    public void cargarPacientes(String archivo) {
+        Vector<Paciente> pacientes = reader.readPatients(archivo);
+        for (Paciente paciente : pacientes) {
+            queue.add(paciente);
+        }
+    }
+
+    public void atenderPacientes() {
+        System.out.println("\nOrden de atención:\n");
+        while (!queue.isEmpty()) {
+            System.out.println(queue.remove());
+        }
+    }
 }
